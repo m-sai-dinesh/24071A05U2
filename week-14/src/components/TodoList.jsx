0fe640f70d9e73d407b8a207aa510dcdcc4d6c50
SAI DINESH MOGAPARTHI <saidineshm24@gmail.com>
2026-02-10 12:29:35 +0530

    Update TodoList.jsx

--- week-14/src/components/TodoList.jsx
+++ week-14/src/components/TodoList.jsx
@@ -1,14 +1,15 @@
 import TodoItem from "./TodoItem";
 
-function TodoList({ todos, toggleTodo, deleteTodo }) {
+function TodoList({ todos, toggleTodo, deleteTodo, updateTodo }) {
   return (
     <div>
-      {todos.map(todo => (
+      {todos.map((todo) => (
         <TodoItem
           key={todo.id}
           todo={todo}
           toggleTodo={toggleTodo}
           deleteTodo={deleteTodo}
+          updateTodo={updateTodo}
         />
       ))}
     </div>
